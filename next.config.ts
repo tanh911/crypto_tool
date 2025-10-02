d225bfe6ae8300b70f1f51aa0654ee67e0854fe9
Tuan Anh <62019384+tanh911@users.noreply.github.com>
2025-10-02 19:25:13 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // Bỏ qua các warning/error khi build trên Netlify
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
